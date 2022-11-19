1f9371ff5be92f3ca3afa7b5036f92fb562967fe
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-20 01:00:17 +0600

    Creating a Simple Calculator

--- cpp_user_input.cpp
+++ cpp_user_input.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main()
 {
+    /*
     int x;
 
     cout<<"Type a number: ";
     cin>>x;
 
     cout<<"Your number is: "<<x;
+    */
+
+    int x, y;
+    int sum;
+
+    // Input
+    cout<<"Type a number: ";
+    cin>>x;
+    cout<<"Type another number: ";
+    cin>>y;
+
+    sum = x + y;
+
+    // Output
+    cout<<"Sum is: "<<sum;
 
     return 0;
 }
