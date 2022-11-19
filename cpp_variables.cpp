c748c3cebc7f88bd73e9ab03d0f6c4e4604b6e6e
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-20 00:41:49 +0600

    C++ Declare Multiple Variables

--- cpp_variables.cpp
+++ cpp_variables.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main()
 {
-    int myNum = 15;
-    cout<<myNum<<endl;
+    //int myNum = 15;
+    //cout<<myNum<<endl;
+    int x = 50, y = 5, z = 50;
+
+    cout << x + y + z;
+
     return 0;
 }
